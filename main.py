6cf53441a911f53ca57689edc67e15e6e6c57942
LimJiHae <91642959+LimJiHea@users.noreply.github.com>
2022-02-14 08:03:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+a_string = "like this"
+a_number = 3
+a_float = 3.12
+a_boolean = False
+a_none = None
+print(type(a_number))
+print(type(a_float))
+print(type(a_boolean))
+print(type(a_none))
+
+
+days = ["Mon","Tue","Wed","Thur","Fri"]
+print(days[3])
+print(type(days))
+print(len(days))
+print("Modn" in days)
+days.append("rrr")
+print(days)
+days.reverse()
+print(days)
+
+days = ("Mon","Tue","Wed","Thur","Fri")
