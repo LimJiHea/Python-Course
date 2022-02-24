0e61f73c4c2892daf9e775ad4ad5429bb1955b20
LimJiHae <91642959+LimJiHea@users.noreply.github.com>
2022-02-24 13:52:00 +0000

    commit

--- stackoverflow.py
+++ stackoverflow.py
@@ -8,7 +8,7 @@ def get_last_page():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
   pages = soup.find("div",{"class":"s-pagination"}).find_all("a")
-  print(pages)
+  #print(pages)
 
 
 
